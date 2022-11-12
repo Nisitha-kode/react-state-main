e8189f77eacbeb92c4d145de54c16f1d98e370b9
NisithaPrakash <nisitha.kodehode@gmail.com>
2022-11-12 12:13:25 +0100

    Added comments

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const imageUrl = 'https://i.imgur.com/fHyEMsl.jpg'
 export default function App () {
   const [img, setImg] = useState()
 
+  //Fetch the image from API and store the image with Usestate
   const fetchImage = async () => {
     const res = await fetch(imageUrl)
     const imageBlob = await res.blob()
@@ -12,9 +13,9 @@ export default function App () {
   }
 
   useEffect(() => {
-    fetchImage()
+    fetchImage() //* call our fetchImage function in a useEffect hook.
   }, [])
-
+  //Use the fetched image inside the component
   return (
     <>
       <img src={img} alt='icons' />
